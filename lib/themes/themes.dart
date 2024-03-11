6fec3d957db6716ae4fb2692004dad0dc769071a
biryuchka <alisa.biryuk@gmail.com>
2024-03-11 16:15:27 +0300

    theme tabbar

--- lib/themes/themes.dart
+++ lib/themes/themes.dart
@@ -16,6 +16,10 @@ ThemeData lightMode = ThemeData(
       fontWeight: FontWeight.bold,
     ),
   ),
+  tabBarTheme: TabBarTheme(
+    labelColor: Colors.grey.shade900,
+    unselectedLabelColor: Colors.grey.shade700,
+  ),
   textTheme: TextTheme(
     bodyMedium: TextStyle(
       color: Colors.grey.shade900,
@@ -32,27 +36,32 @@ ThemeData lightMode = ThemeData(
   ),
 );
 
+
 ThemeData darkMode = ThemeData(
-    scaffoldBackgroundColor: Colors.grey.shade900,
+  scaffoldBackgroundColor: Colors.grey.shade900,
+  iconTheme: IconThemeData(
+    color: Colors.grey.shade400,
+  ),
+  appBarTheme: AppBarTheme(
+    color: Colors.grey.shade800,
     iconTheme: IconThemeData(
       color: Colors.grey.shade400,
     ),
-    appBarTheme: AppBarTheme(
-      color: Colors.grey.shade800,
-      iconTheme: IconThemeData(
-        color: Colors.grey.shade400,
-      ),
-      titleTextStyle: TextStyle(
-        color: Colors.grey.shade400,
-        fontSize: 20,
-        fontWeight: FontWeight.bold,
-      ),
+    titleTextStyle: TextStyle(
+      color: Colors.grey.shade400,
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
     ),
-    textTheme: TextTheme(
-      bodyMedium: TextStyle(
-        color: Colors.grey.shade400,
-      ),
+  ),
+  textTheme: TextTheme(
+    bodyMedium: TextStyle(
+      color: Colors.grey.shade400,
     ),
+  ),
+  tabBarTheme: TabBarTheme(
+    labelColor: Colors.grey.shade300,
+    unselectedLabelColor: Colors.grey.shade500,
+  ),
   cardTheme: CardTheme(
     color: Colors.grey.shade800,
   ),
